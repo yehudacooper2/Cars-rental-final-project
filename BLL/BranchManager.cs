be0627e93569fb2c9a8c28a08d5afc5003e5cff1
yehudacooper2 <38185234+yehudacooper2@users.noreply.github.com>
2018-09-06 23:36:44 +0300

    Add files via upload

---
+++ BLL/BranchManager.cs
@@ -0,0 +1,168 @@
+﻿using DAL;
+using BOL;
+using System;
+using System.Linq;
+
+namespace BLL
+{
+    static public class BranchManager
+    {
+        /// <summary>
+        /// SelectAllBranches reads all the Branches from the DB by the EF ref
+        /// and maps the DAL objects to BOL objects
+        /// </summary>
+        static public BranchModel[] SelectAllBranches()
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    return ef.Branches.Select(dbBranch => new BranchModel
+                    {
+                        BranchAddress = dbBranch.address,
+                        BranchLatitude = dbBranch.latitude,
+                        BranchLongitude = dbBranch.longitude,
+                        BranchName = dbBranch.branchName,
+                    }).ToArray();
+
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// SelectBranchByName selects a specific Branch from the DB by the EF ref
+        /// by the `branchName` parameter
+        /// and maps the DAL object to BOL object
+        /// </summary>
+        static public BranchModel SelectBranchByName(string branchName)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    Branch selectedBranch = ef.Branches.FirstOrDefault(dbUser => dbUser.branchName == branchName);
+                    if (selectedBranch == null)
+                        return null;
+
+                    return new BranchModel
+                    {
+                        BranchAddress = selectedBranch.address,
+                        BranchLatitude = selectedBranch.latitude,
+                        BranchLongitude = selectedBranch.longitude,
+                        BranchName = selectedBranch.branchName,
+
+                    };
+
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// InsertBranch inserts a new Branch to the DB by the EF ref
+        /// maps the `newBranch` parameter (BOL object) to a `Branch` (DAL object)
+        /// and returns bool value - if the action was success
+        /// </summary>
+        static public bool InsertBranch(BranchModel newBranch)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+                    Branch newDbBranch = new Branch
+                    {
+                        address = newBranch.BranchAddress,
+                        latitude = newBranch.BranchLatitude,
+                        longitude = newBranch.BranchLongitude,
+                        branchName = newBranch.BranchName,
+
+                    };
+
+                    ef.Branches.Add(newDbBranch);
+                    ef.SaveChanges();
+                    return true;
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// UpdateBranchByName updates a specific Branch from the DB by the EF ref
+        /// by the `branchName` parameter
+        /// and returns bool value - if the action was success
+        /// </summary>
+        static public bool UpdateBranchByName(string branchName, BranchModel newBranch)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    Branch selectedBranch = ef.Branches.FirstOrDefault(dbBranch => dbBranch.branchName == branchName);
+                    if (selectedBranch == null)
+                        return false;
+
+                    selectedBranch.address = newBranch.BranchAddress;
+                    selectedBranch.latitude = newBranch.BranchLatitude;
+                    selectedBranch.longitude = newBranch.BranchLongitude;
+                    selectedBranch.branchName = newBranch.BranchName;
+
+
+                    ef.SaveChanges();
+                    return true;
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// DeleteBranchByName delete a specific Branch from the DB by the EF ref
+        /// by the `branchName` parameter
+        /// and returns bool value - if the action was success
+        /// </summary>
+        static public bool DeleteBranchByName(string branchName)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    Branch selectedBranch = ef.Branches.FirstOrDefault(dbBranch => dbBranch.branchName == branchName);
+                    if (selectedBranch == null)
+                        return false;
+
+                    ef.Branches.Remove(selectedBranch);
+                    ef.SaveChanges();
+                    return true;
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BLL/CarTypeManager.cs
@@ -0,0 +1,178 @@
+﻿using DAL;
+using BOL;
+using System;
+using System.Linq;
+
+namespace BLL
+{
+    static public class CarTypeManager
+    {
+        /// <summary>
+        /// SelectAllCarTypes reads all the CarTypes from the DB by the EF ref
+        /// and maps the DAL objects to BOL objects
+        /// </summary>
+        static public CarTypeModel[] SelectAllCarTypes()
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    return ef.CarTypes.Select(dbUser => new CarTypeModel
+                    {
+                        //    CarTypeId = dbUser.carTypeId,
+                        Manufacturer = dbUser.manufacturer,
+                        Model = dbUser.model,
+                        DailyCost = dbUser.dailyCost,
+                        DayDelayCost = dbUser.dayDelayCost,
+                        ManufactureYear = dbUser.manufactureYear,
+                        Gear = dbUser.gear,
+
+                    }).ToArray();
+
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// SelectCarTypeByModel selects a specific CarType from the DB by the EF ref
+        /// by the `carTypeModelName` parameter
+        /// and maps the DAL object to BOL object
+        /// </summary>
+        static public CarTypeModel SelectCarTypeByModel(string carModel)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    CarType selectedCarType = ef.CarTypes.FirstOrDefault(dbCarType => dbCarType.model == carModel);
+                    if (selectedCarType == null)
+                        return null;
+
+                    return new CarTypeModel
+                    {
+                        //  CarTypeId = selectedCarType.carTypeId,
+                        Manufacturer = selectedCarType.manufacturer,
+                        Model = selectedCarType.model,
+                        DailyCost = selectedCarType.dailyCost,
+                        DayDelayCost = selectedCarType.dayDelayCost,
+                        ManufactureYear = selectedCarType.manufactureYear,
+                        Gear = selectedCarType.gear,
+
+                    };
+
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// InsertCarType inserts a new CarType to the DB by the EF ref
+        /// maps the `newCarType` parameter (BOL object) to a `CarType` (DAL object)
+        /// and returns bool value - if the action was success
+        /// </summary>
+        static public bool InsertCarType(CarTypeModel newCarType)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+                    CarType newDbCarType = new CarType
+                    {
+                        manufacturer = newCarType.Manufacturer,
+                        model = newCarType.Model,
+                        dailyCost = newCarType.DailyCost,
+                        dayDelayCost = newCarType.DayDelayCost,
+                        manufactureYear = newCarType.ManufactureYear,
+                        gear = newCarType.Gear,
+                    };
+
+                    ef.CarTypes.Add(newDbCarType);
+                    ef.SaveChanges();
+                    return true;
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// UpdateCarTypeByName updates a specific CarType from the DB by the EF ref
+        /// by the `carTypeModelName` parameter
+        /// and returns bool value - if the action was success
+        /// </summary>
+        static public bool UpdateCarTypeByModel(string carModel, CarTypeModel newCarType)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    CarType selectedCarType = ef.CarTypes.FirstOrDefault(dbCarType => dbCarType.model == carModel);
+                    if (selectedCarType == null)
+                        return false;
+
+                    //  selectedCarType.carTypeId = newCarType.CarTypeId;
+                    selectedCarType.manufacturer = newCarType.Manufacturer;
+                    selectedCarType.model = newCarType.Model;
+                    selectedCarType.dailyCost = newCarType.DailyCost;
+                    selectedCarType.dayDelayCost = newCarType.DayDelayCost;
+                    selectedCarType.manufactureYear = newCarType.ManufactureYear;
+                    selectedCarType.gear = newCarType.Gear;
+
+                    ef.SaveChanges();
+                    return true;
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// DeleteCarTypeByModel delete a specific CarType from the DB by the EF ref
+        /// by the `carTypeModelName` parameter
+        /// and returns bool value - if the action was success
+        /// </summary>
+        static public bool DeleteCarTypeByModel(string carModel)
+        {
+            try
+            {
+                using (CarsRentalEntities ef = new CarsRentalEntities())
+                {
+
+                    CarType selectedCarType = ef.CarTypes.FirstOrDefault(dbCarType => dbCarType.model == carModel);
+                    if (selectedCarType == null)
+                        return false;
+
+                    ef.CarTypes.Remove(selectedCarType);
+                    ef.SaveChanges();
+                    return true;
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
